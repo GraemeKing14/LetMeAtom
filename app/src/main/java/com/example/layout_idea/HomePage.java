97d06198a089d5d9e94848da732814e3802ed9dd
Graeme <rkb19214@uni.strath.ac.uk>
2020-07-28 11:57:34 +0100

    Title and back buttons added for elec model

--- app/src/main/java/com/example/layout_idea/HomePage.java
+++ app/src/main/java/com/example/layout_idea/HomePage.java
@@ -2,17 +2,12 @@ package com.example.layout_idea;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.layout_idea.Electricity.ElecHome;
