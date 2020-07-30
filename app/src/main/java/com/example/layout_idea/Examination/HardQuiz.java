29832a50d4c8d9a6cb91a7725c6f2ca18962f01a
Graeme <rkb19214@uni.strath.ac.uk>
2020-07-30 18:18:56 +0100

    Fully hard test functionality added. Series of classes involving databases added in order to contain questions.

---
+++ app/src/main/java/com/example/layout_idea/Examination/HardQuiz.java
@@ -0,0 +1,176 @@
+package com.example.layout_idea.Examination;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.layout_idea.DatabaseHelper;
+import com.example.layout_idea.Questions;
+import com.example.layout_idea.QuizDatabaseHelper;
+import com.example.layout_idea.R;
+import com.example.layout_idea.SharedPrefs;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HardQuiz extends AppCompatActivity {
+
+    SharedPrefs sharedPref;
+    private QuizDatabaseHelper dbQuestions;
+    private TextView question;
+    private TextView questionScore;
+    private TextView questionTimer;
+    private TextView questionNumber;
+    private RadioGroup rg;
+    private RadioButton answerA;
+    private RadioButton answerB;
+    private RadioButton answerC;
+    private RadioButton answerD;
+    private Button nextButton;
+
+    private ColorStateList defaultButton;
+
+    private List<Questions> questionsList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Questions currentQuestion;
+
+    private int score;
+    private boolean answered;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        sharedPref = new SharedPrefs(this);
+        if(sharedPref.loadNightMode()==true) {
+            setTheme(R.style.DarkTheme);
+        }
+        else {
+            setTheme(R.style.AppTheme);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hard_quiz);
+        setUpUi();
+        showNextQuestion();
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!answered) {
+                    if (answerA.isChecked() || answerB.isChecked() || answerC.isChecked() || answerD.isChecked()) {
+                        checkAnswer();
+                    } else {
+                        Toast.makeText(HardQuiz.this, "Please select an answer", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    showNextQuestion();
+                }
+            }
+        });
+
+    }
+
+    public void setUpUi() {
+        question = findViewById(R.id.questionContent);
+        questionScore = findViewById(R.id.questionScore);
+        questionTimer = findViewById(R.id.questionTimer);
+        questionNumber = findViewById(R.id.questionNumber);
+        rg = findViewById(R.id.radioGroup);
+        answerA = findViewById(R.id.answerA);
+        answerB = findViewById(R.id.answerB);
+        answerC = findViewById(R.id.answerC);
+        answerD = findViewById(R.id.answerD);
+        nextButton = findViewById(R.id.nextButton);
+
+        defaultButton = answerA.getTextColors();
+
+        dbQuestions = new QuizDatabaseHelper(this);
+        questionsList = dbQuestions.getAllQuestions();
+        questionCountTotal = questionsList.size();
+        Collections.shuffle(questionsList);
+    }
+
+    private void showNextQuestion() {
+        answerA.setTextColor(defaultButton);
+        answerB.setTextColor(defaultButton);
+        answerC.setTextColor(defaultButton);
+        answerD.setTextColor(defaultButton);
+        rg.clearCheck();
+
+        if (questionCounter < questionCountTotal) {
+            currentQuestion = questionsList.get(questionCounter);
+
+            question.setText(currentQuestion.getQuestion());
+            answerA.setText(currentQuestion.getOption1());
+            answerB.setText(currentQuestion.getOption2());
+            answerC.setText(currentQuestion.getOption3());
+            answerD.setText(currentQuestion.getOption4());
+
+            questionCounter++;
+            questionNumber.setText("Question:" + questionCounter + "/" + questionCountTotal);
+            answered = false;
+            nextButton.setText("Confirm");
+        } else {
+            finishQuiz();
+        }
+    }
+
+    private void checkAnswer() {
+        answered = true;
+
+        RadioButton rbSelected = findViewById(rg.getCheckedRadioButtonId());
+        int answerNr = rg.indexOfChild(rbSelected) + 1;
+
+        if (answerNr == currentQuestion.getAnswerNr()) {
+            score++;
+            questionScore.setText("Score: " + score);
+        }
+        showSolution();
+    }
+
+    private void showSolution() {
+        answerA.setTextColor(Color.RED);
+        answerB.setTextColor(Color.RED);
+        answerC.setTextColor(Color.RED);
+        answerD.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNr()) {
+            case 1:
+                answerA.setTextColor(Color.GREEN);
+                question.setText("Answer A is Correct!");
+                break;
+            case 2:
+                answerB.setTextColor(Color.GREEN);
+                question.setText("Answer B is Correct!");
+                break;
+            case 3:
+                answerC.setTextColor(Color.GREEN);
+                question.setText("Answer C is Correct!");
+                break;
+            case 4:
+                answerD.setTextColor(Color.GREEN);
+                question.setText("Answer D is Correct!");
+                break;
+        }
+
+        if (questionCounter < questionCountTotal) {
+            nextButton.setText("Next");
+        } else {
+            nextButton.setText("Finish");
+        }
+    }
+
+    private void finishQuiz() {
+        finish();
+    }
+}
--- app/src/main/java/com/example/layout_idea/Examination/examDifficulty.java
+++ app/src/main/java/com/example/layout_idea/Examination/examDifficulty.java
@@ -2,7 +2,10 @@ package com.example.layout_idea.Examination;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.layout_idea.R;
 import com.example.layout_idea.SharedPrefs;
@@ -10,6 +13,9 @@ import com.example.layout_idea.SharedPrefs;
 public class examDifficulty extends AppCompatActivity {
 
     SharedPrefs sharedPref;
+    private Button easy;
+    private Button medium;
+    private Button hard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,19 @@ public class examDifficulty extends AppCompatActivity {
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_difficulty);
+        setUpUi();
+    }
+
+    public void setUpUi() {
+        easy = findViewById(R.id.easyButton);
+        medium = findViewById(R.id.mediumButton);
+        hard = findViewById(R.id.hardButton);
+
+        hard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(examDifficulty.this, HardQuiz.class));
+            }
+        });
     }
 }
